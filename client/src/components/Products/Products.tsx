31de128ce882ad04dc761096d2fcb75c5743b4fe
NurulHakim <nurulhakim2810@gmail.com>
2020-05-16 21:55:08 +0700

    interface Props state in products.tsx

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -7,3 +7,17 @@ import { ICatalogProduct } from '@typings/state/index';
 import FiltersList from '../FiltersList';
 import Product from '../Product';
 import '@styles/Products.css';
+
+interface Props {
+    catalogLoaded: boolean;
+    catalog: ICatalogProduct[];
+    sortBy: string;
+    initCatalog: () => void;
+    clearFilters: () => void;
+    setSortBy: (value: string) => void;
+  }
+  
+  interface State {
+    drawerOpen: boolean;
+    value: string;
+  }
